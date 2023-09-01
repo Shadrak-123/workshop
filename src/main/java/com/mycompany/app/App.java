2940f2081b7dc12bc2abcf9de5edfa8efa0c6679
Shadrak-123 <228T5A4204@diet.ac.in>
2023-09-01 11:03:11 +0530

    First

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Shadrak's World Thanks for coming" );
+        System.out.println( "Welcome to Shadrak's World Thanks for coming to learn something" );
     }
 }
